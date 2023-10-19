0edcf43c590fd37dc05846b53d00008d9123f2b5
Rahul Dev Garg <34365102+rahuldevgarg@users.noreply.github.com>
2023-10-19 20:27:35 +0530

    added cards list from json

--- src/components/debit-card/DebitCard.js
+++ src/components/debit-card/DebitCard.js
@@ -2,8 +2,16 @@ import React from "react"
 import "./DebitCard.css"
 import cards from "../../cards.json"
 export const DebitCard = () => {
+	const availableCards = [];
 
-
+	for (let i = 0; i < cards.length; i++) {
+		availableCards.push(
+			<div className="list-card" data-testid={`list-card-${i}`} >
+				<p className="list-card-title">Card {`${i+1}`}</p>
+				</div>
+			);
+	}
+	
 	return (
 		<div className="mt-50 layout-column justify-content-center align-items-center" >
 			<div className="card outlined" style={{ width: '1000px' }}>
@@ -24,18 +32,7 @@ export const DebitCard = () => {
 				<div>
 					<h3 style={{ textAlign: "center" }}>Cards List</h3>
 					<div className="debit-card-list" data-testid="debit-card-list">
-						
-						<div className="list-card" data-testid="list-card-0">
-							<p className="list-card-title">Card 1</p>
-						</div>
-						
-						
-						<div className="list-card" data-testid="list-card-2"><p className="list-card-title">Card 2</p></div>
-						<div className="list-card" data-testid="list-card-3"><p className="list-card-title">Card 3</p></div>
-						<div className="list-card" data-testid="list-card-4"><p className="list-card-title">Card 4</p></div>
-
-
-
+						{availableCards}
 					</div>
 				</div>
 			</div>
