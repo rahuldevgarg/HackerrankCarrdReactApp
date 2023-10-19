55c5ab16504154af19113dd038caa69fe4e6cc66
Rahul Dev Garg <34365102+rahuldevgarg@users.noreply.github.com>
2023-10-19 21:50:45 +0530

    complete task

--- src/components/debit-card/DebitCard.js
+++ src/components/debit-card/DebitCard.js
@@ -3,47 +3,61 @@ import "./DebitCard.css";
 import cards from "../../cards.json";
 export const DebitCard = () => {
   const availableCards = [];
-  var [selectedCard,setSelectedCard] = useState({
+  var [showEnc, setShowEnc] = useState(true);
+  var [selectedCard, setSelectedCard] = useState({
     number: "XXXX XXXX XXXX XXXX",
     expiry: "MM/YY",
     cvv: "CVV",
     name: "HOLDER NAME",
     bank: "Bank Name",
   });
+  function changeEntryption() {
+    setShowEnc(!showEnc);
+    var card = {
+      number: getCardNumber(selectedCard),
+      expiry: getCardExpiry(selectedCard),
+      cvv: getCardCVV(selectedCard),
+      name: getCardHolderName(selectedCard),
+      bank: selectedCard["bank"],
+      id: selectedCard["id"],
+    };
+    setSelectedCard(card);
+  }
   function wordcount(str) {
     let c = 0;
     let str1 = str.split(" ");
 
     for (let i = 0; i < str1.length; i++) {
       if (str1.length === 0) {
-		  c = 0;
+        c = 0;
       }
       if (str1[i] !== "") {
         c++;
       }
-	}
-	  return c;
+    }
+    return c;
   }
-	function getEncName(name) {
-		let words = wordcount(name);
-		var encName = '';
-		for (let i = 0; i < words; i++){
-			if (i === words - 1) {
-				encName = encName+'XXXX'
-			}
-			else {
-				encName = encName+'XXXX '
-			}
-		}
-		return encName;
-	}
-	function setSelectedCardVal(cardId) {
-		var encVal = cards[cardId];
-		encVal['number'] = `${encVal['number'].substring(0,4)} XXXX XXXX XXXX`;
-		encVal['expiry'] = 'XX/XX';
-		encVal['cvv'] = 'XXX';
-		encVal['name'] = `${getEncName(encVal['name'])}`;
-		setSelectedCard({...encVal,id:cardId});
+  function getEncName(name) {
+    let words = wordcount(name);
+    var encName = "";
+    for (let i = 0; i < words; i++) {
+      if (i === words - 1) {
+        encName = encName + "XXXX";
+      } else {
+        encName = encName + "XXXX ";
+      }
+    }
+    return encName;
+  }
+
+  function setSelectedCardVal(cardId) {
+    var encVal = { ...cards[cardId] };
+    encVal["number"] = `${encVal["number"].substring(0, 4)} XXXX XXXX XXXX`;
+    encVal["expiry"] = "XX/XX";
+    encVal["cvv"] = "XXX";
+    encVal["name"] = `${getEncName(encVal["name"])}`;
+    setSelectedCard({ ...encVal, id: cardId });
+    setShowEnc(false);
   }
   for (let i = 0; i < cards.length; i++) {
     availableCards.push(
@@ -51,10 +65,46 @@ export const DebitCard = () => {
         className="list-card"
         data-testid={`list-card-${i}`}
         onClick={() => setSelectedCardVal(i)}
+        key={i}
       >
         <p className="list-card-title">Card {`${i + 1}`}</p>
       </div>
     );
+  }
+  function getCardNumber(card) {
+    if (showEnc) {
+      return `${cards[card["id"]]["number"].substring(0, 4)} XXXX XXXX XXXX`;
+    }
+    var number = cards[card["id"]]["number"];
+    var formattedNumber = number.match(/.{1,4}/g) || [];
+
+    var numberT = "";
+    for (let i = 0; i < formattedNumber.length; i++) {
+      if (i === formattedNumber.length - 1) {
+        numberT += formattedNumber[i];
+      } else {
+        numberT += formattedNumber[i] + " ";
+      }
+    }
+    return numberT;
+  }
+  function getCardHolderName(card) {
+    if (showEnc) {
+      return getEncName(cards[card["id"]]["name"]);
+    }
+    return cards[card["id"]]["name"];
+  }
+  function getCardExpiry(card) {
+    if (showEnc) {
+      return "XX/XX";
+    }
+    return cards[card["id"]]["expiry"];
+  }
+  function getCardCVV(card) {
+    if (showEnc) {
+      return "XXX";
+    }
+    return cards[card["id"]]["cvv"];
   }
 
   return (
@@ -63,7 +113,11 @@ export const DebitCard = () => {
         <div data-testid="debit-card">
           <h3 style={{ textAlign: "center" }}>Card Details</h3>
           <br />
-          <div className="debit-card-body" data-testid="debit-card-body">
+          <div
+            className="debit-card-body"
+            data-testid="debit-card-body"
+            onClick={() => changeEntryption()}
+          >
             <p className="debit-card-bank" data-testid="debit-card-bank-name">
               {selectedCard["bank"]}
             </p>
